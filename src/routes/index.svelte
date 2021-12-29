792b94ddb30f19100f8c31cc333a80cc85fab97d
Chetan Sarva <chetan@pixelcop.net>
2021-12-28 21:25:14 -0500

    simple tailwind test case

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-lg">Welcome to SvelteKit</h1>
+<p class="border-2 m-4 p-4">
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
